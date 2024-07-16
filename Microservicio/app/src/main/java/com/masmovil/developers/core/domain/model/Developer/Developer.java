1b9282a6be469d91519272f22053fd709eb09360
nico-arroyo <nicolas.arroyo.geldhof@gmail.com>
2024-07-16 12:22:57 +0200

    Update Developer.java

--- Microservicio/app/src/main/java/com/masmovil/developers/core/domain/model/Developer/Developer.java
+++ Microservicio/app/src/main/java/com/masmovil/developers/core/domain/model/Developer/Developer.java
@@ -1,4 +1,4 @@
-package com.masmovil.developers.core.domain.model.Developer;
+package com.masmovil.developers.core.domain.model.developer;
 
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
@@ -7,16 +7,23 @@ import java.util.List;
 
 public class Developer {
 
+    private final DeveloperId id;
     private final DeveloperName name;
     private final List<String> skills;
 
-    private Developer(Builder builder) {
+    private Developer(Builder builder) throws IllegalArgumentException {
+        if (builder.skills == null || builder.skills.isEmpty()) {
+            throw new IllegalArgumentException("Skills cannot be null or empty");
+        }
+
+        id = builder.id;
         name = builder.name;
         skills = builder.skills;
     }
 
     public static Developer create(JsonObject data) {
         return builder()
+                .withId(DeveloperId.of(data.getString("id")))
                 .withName(DeveloperName.fromJson(data.getJsonObject("name")))
                 .withSkills(data.getJsonArray("skills").getList())
                 .build();
@@ -28,15 +35,22 @@ public class Developer {
 
     public JsonObject toJson() {
         return new JsonObject()
+                .put("id", id.toString())
                 .put("name", name.toJson())
                 .put("skills", new JsonArray(skills));
     }
 
     public static final class Builder {
+        private DeveloperId id;
         private DeveloperName name;
         private List<String> skills;
 
         private Builder() {}
+
+        public Builder withId(DeveloperId id) {
+            this.id = id;
+            return this;
+        }
 
         public Builder withName(DeveloperName name) {
             this.name = name;
@@ -51,5 +65,29 @@ public class Developer {
         public Developer build() {
             return new Developer(this);
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) {
+            return false;
+        }
+        if (!this.getClass().equals(o.getClass())) {
+            return false;
+        }
+        Developer other = (Developer) o;
+        return this.id.equals(other.id);
+    }
+
+    public DeveloperId getId() {
+        return id;
+    }
+
+    public DeveloperName getName() {
+        return name;
+    }
+
+    public List<String> getSkills() {
+        return skills;
     }
 }
