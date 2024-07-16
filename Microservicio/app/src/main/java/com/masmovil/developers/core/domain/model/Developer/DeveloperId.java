901b820d9cabeb14d311902034597bf7b0b98f3f
nico-arroyo <nicolas.arroyo.geldhof@gmail.com>
2024-07-16 12:23:16 +0200

    Update DeveloperId.java

--- Microservicio/app/src/main/java/com/masmovil/developers/core/domain/model/Developer/DeveloperId.java
+++ Microservicio/app/src/main/java/com/masmovil/developers/core/domain/model/Developer/DeveloperId.java
@@ -1,13 +1,15 @@
-package com.masmovil.developers.core.domain.model.Developer;
+package com.masmovil.developers.core.domain.model.developer;
 
 import java.util.UUID;
 
 public class DeveloperId {
     private final UUID id;
 
-    private DeveloperId(UUID id) {
-        this.id = id;
-    }
+    public DeveloperId(UUID id) throws IllegalArgumentException {
+        if (id == null || id.toString().isEmpty()) {
+            throw new IllegalArgumentException("Id cannot be null or empty");
+        }
+        this.id = id;    }
 
     public static DeveloperId create(){
         return new DeveloperId(UUID.randomUUID());
@@ -15,6 +17,23 @@ public class DeveloperId {
 
     public static DeveloperId of(String id){
         return new DeveloperId(UUID.fromString(id));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) {
+            return false;
+        }
+        if (!this.getClass().equals(o.getClass())) {
+            return false;
+        }
+        DeveloperId other = (DeveloperId) o;
+        return this.id.equals(other.id);
+    }
+
+    @Override
+    public String toString() {
+        return id.toString();
     }
 
     public UUID getId() {
