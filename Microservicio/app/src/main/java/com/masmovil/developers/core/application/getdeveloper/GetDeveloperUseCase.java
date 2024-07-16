f367b713edb6272d80b4c167b7df2ce6aaf14186
nico-arroyo <nicolas.arroyo.geldhof@gmail.com>
2024-07-16 12:22:32 +0200

    Update GetDeveloperUseCase.java

--- Microservicio/app/src/main/java/com/masmovil/developers/core/application/getdeveloper/GetDeveloperUseCase.java
+++ Microservicio/app/src/main/java/com/masmovil/developers/core/application/getdeveloper/GetDeveloperUseCase.java
@@ -1,20 +1,35 @@
-package com.masmovil.developers.core.application.getdeveloper;
+package com.masmovil.developers.core.application.getdevelopers;
 
 import com.masmovil.developers.core.domain.repository.DeveloperRepository;
 import io.reactivex.rxjava3.core.Single;
+import io.vertx.core.json.JsonObject;
 
-public class GetDeveloperUseCase {
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class GetDevelopersUseCase {
 
     private final DeveloperRepository repository;
 
-    public GetDeveloperUseCase(DeveloperRepository repository) {
+    public GetDevelopersUseCase(DeveloperRepository repository) {
         this.repository = repository;
     }
 
-    public Single<GetDeveloperResponse> execute(GetDeveloperRequest request) {
+    public Single<GetDevelopersResponse> execute(GetDevelopersRequest request) {
         String name = request.getName();
-        return repository.getByName(name)
-                .map(developer -> new GetDeveloperResponse(developer.toJson()))
-                .defaultIfEmpty(new GetDeveloperResponse("Developer not found"));
+        String skill = request.getSkill();
+        return repository.getByQuery(name, skill)
+                .map (developers -> {
+                    if (developers.isEmpty()) {
+                        return new GetDevelopersResponse("No developers found matching the criteria");
+                    } else {
+                        List<JsonObject> developerJsons = developers.stream()
+                                .map(developer -> developer.toJson())
+                                .collect(Collectors.toList());
+                        return new GetDevelopersResponse(developerJsons);
+                    }
+                });
+
+
     }
 }
